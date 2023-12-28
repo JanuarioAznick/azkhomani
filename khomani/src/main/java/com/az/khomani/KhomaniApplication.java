b7b6ea62d61c807b22b7f53d800e31c9e39505ae
JanuarioAznick <januario-jose@outlook.com>
2023-12-28 18:57:25 +0200

    Backend Project created

---
+++ khomani/src/main/java/com/az/khomani/KhomaniApplication.java
@@ -0,0 +1,13 @@
+package com.az.khomani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhomaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhomaniApplication.class, args);
+    }
+
+}
---
+++ khomani/src/test/java/com/az/khomani/KhomaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.khomani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhomaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
